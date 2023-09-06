b6b17e4fc9ad8c45505707c0f9590677938fe438
Kovács Zoltán <kovacs.zoltan@softc.hu>
2023-09-06 12:15:20 +0200

    Megjegyzések

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -9,8 +9,8 @@ use Socialite;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Http\Request;
 use Http;
-use GuzzleHttp\Psr7;
-use GuzzleHttp\Exception\ClientException;
+//use GuzzleHttp\Psr7;
+//use GuzzleHttp\Exception\ClientException;
 
 class LoginController extends Controller {
 
@@ -24,17 +24,16 @@ class LoginController extends Controller {
     protected $_url_assigned_user = "https://dev-87493811.okta.com/api/v1/apps";
 
     /**
-     * Redirect the user to the Okta authentication page.
+     * A felhasználó átirányítása az Okta hitelesítési oldalra.
      *
      * @return \Illuminate\Http\Response
      */
     public function redirectToProvider() {
-        return Socialite::driver('okta')
-                        ->redirect();
+        return Socialite::driver('okta')->redirect();
     }
 
     /**
-     * Obtain the user information from Okta.
+     * Szerezze be a felhasználói információkat az Oktától.
      *
      * @return \Illuminate\Http\Response
      */
@@ -67,11 +66,19 @@ class LoginController extends Controller {
         return redirect('/home');
     }
 
+    /**
+     * Kiléptetés a helyi rendszerből
+     * @return type
+     */
     public function logout() {
         Auth::logout();
         return redirect('/');
     }
 
+    /**
+     * Kiléptetés az Okta rendszerből
+     * @return type
+     */
     public function sso_logout() {
         $idToken = Auth::user()->id_token;
 
@@ -82,36 +89,50 @@ class LoginController extends Controller {
         return redirect($logoutUrl);
     }
 
+    /**
+     * Új felhasználó regisztrálása.
+     * @param Request $request
+     * @return type
+     */
     public function add_user(Request $request) {
-
+        // Új felhasználó adatai
         $new_user = $request->all();
-// insert  user        
+        
+        // adatok küldése az Okta rendszerbe
         try {
             $response = Http::withHeaders([
-                        "Accept" => "application/json",
-                        "Content-Type" => "application/json",
-                        "Authorization" => "SSWS " . $this->_api_token_secret])
-                    ->withUrlParameters(["activate" => true])
-                    ->withBody(json_encode(["profile" => [
+                "Accept" => "application/json",
+                "Content-Type" => "application/json",
+                "Authorization" => "SSWS " . $this->_api_token_secret
+            ])
+                ->withUrlParameters(["activate" => true])
+                ->withBody(json_encode(
+                    [
+                        "profile" => [
                             "firstName" => $new_user['first_name'],
                             "lastName" => $new_user['last_name'],
                             "email" => $new_user['email'],
-                            "login" => $new_user['email']],
-                        "credentials" => ["password" => ["value" => $new_user['password']]]]))
-                    ->post($this->_url_add_user_activate);
+                            "login" => $new_user['email']
+                        ],
+                        "credentials" => [
+                            "password" => ["value" => $new_user['password']]
+                        ]
+                    ]
+                ))->post($this->_url_add_user_activate);
         } catch (\Exception $e) {
             \Log::info($e);
             return response()->json(['success' => 0]);
         }
 
 
-// get user id for assigned to app  
+        // felhasználói azonosító beszerzése az alkalmazáshoz való hozzárendeléshez  
         try {
-            $response = Http::withHeaders([
-                        "Accept" => "application/json",
-                        "Content-Type" => "application/json",
-                        "Authorization" => "SSWS " . $this->_api_token_secret
-                    ])->get($this->_url_get_user . '/' . $new_user['email']);
+            $response = Http::withHeaders(
+                [
+                    "Accept" => "application/json",
+                    "Content-Type" => "application/json",
+                    "Authorization" => "SSWS " . $this->_api_token_secret
+                ])->get($this->_url_get_user . '/' . $new_user['email']);
 
             $response_body = json_decode($response->getBody()->getContents());
             \Log::info(print_r($response_body, true));
@@ -121,16 +142,20 @@ class LoginController extends Controller {
             return response()->json(['success' => 1]);
         }
 
-// assigned user to app        
+        // Felhasználó hozzárendelése az alkalmazáshoz
         try {
-            $response = Http::withHeaders([
-                        "Accept" => "application/json",
-                        "Content-Type" => "application/json",
-                        "Authorization" => "SSWS " . $this->_api_token_secret])
-                    ->withBody(json_encode(["id" => $response_data['id'],
+            $response = Http::withHeaders(
+                [
+                    "Accept" => "application/json",
+                    "Content-Type" => "application/json",
+                    "Authorization" => "SSWS " . $this->_api_token_secret
+                ])->withBody(json_encode(
+                    [
+                        "id" => $response_data['id'],
                         "scope" => "USER",
-                        "credentials" => ["userName" => $new_user['email']]]))
-                    ->post($this->_url_assigned_user . '/' . $this->_client_id . '/users');
+                        "credentials" => ["userName" => $new_user['email']]
+                    ]
+                ))->post($this->_url_assigned_user . '/' . $this->_client_id . '/users');
 
             $response_body2 = json_decode($response->getBody()->getContents());
             \Log::info(print_r($response_body2, true));
@@ -146,11 +171,12 @@ class LoginController extends Controller {
 
         try{
             // get current user
-             $response = Http::withHeaders([
-                            "Accept" => "application/json",
-                            "Content-Type" => "application/json",
-                            "Authorization" => "SSWS " . $this->_api_token_secret
-                        ])->get($this->_url_get_user . '/' . auth()->user()->email);
+             $response = Http::withHeaders(
+                [
+                    "Accept" => "application/json",
+                    "Content-Type" => "application/json",
+                    "Authorization" => "SSWS " . $this->_api_token_secret
+                ])->get($this->_url_get_user . '/' . auth()->user()->email);
 
             $response_body = json_decode($response->getBody()->getContents());
             \Log::info(print_r($response_body, true));
